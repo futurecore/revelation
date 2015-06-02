5ddf4810931f1f3db362630ca1dfee2e53bfcfc6
snim2 <mount.sarah@gmail.com>
2015-06-02 18:31:40 +0100

    Can mix 16 and 32bit instructions. See Issue on Cornell fork: https://github.com/cornell-brg/pydgin/issues/24

--- test/test_sim.py
+++ test/test_sim.py
@@ -61,3 +61,20 @@ def test_bcond32():#
     epiphany.run()
     assert epiphany.state.rf[1] == 8 + 0b01010101010
     assert epiphany.state.pc == 12
+
+
+def test_add32_nop16_sub32():
+    from pydgin.utils import trim_32
+    instructions = [opcode_factory.int_arith32_immediate('add', 1,0, 0b01010101010),
+                    opcode_factory.nop16(),
+                    opcode_factory.int_arith32_immediate('sub', 1, 0, 0b01010101010),
+                    ]
+    epiphany = Epiphany()
+    epiphany.init_test_state(instructions)
+    epiphany.state.rf[0] = 0
+    epiphany.run()
+    assert epiphany.state.rf[1] == trim_32(0 - 0b01010101010)
+    assert epiphany.state.AZ == 0  # Zero
+    assert epiphany.state.AN == 1  # Negative
+    assert epiphany.state.AC == 1  # Borrow, take from utility function. CHECK THIS.
+    assert epiphany.state.pc == 12
