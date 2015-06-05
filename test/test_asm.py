bd834fac6b9913540c0f51fd9ee43fa43755b08a
snim2 <mount.sarah@gmail.com>
2015-06-05 10:18:23 +0100

    ASM now tests PC. TODO: Parse registers and memory.

--- test/test_asm.py
+++ test/test_asm.py
@@ -1,18 +1,27 @@
 import os
+import os.path
 import subprocess
 import pytest
 
+from epiphany.test.machine import StateChecker
 
-@pytest.mark.parametrize("asm", ['epiphany/test/asm/nop.elf',
-                                 'epiphany/test/asm/add.elf'])
-def test_asm_nop(asm):
-    assert '/home/snim2/Desktop/working/futurecore/pydgin' == os.getcwd()
+asm_dir = os.path.join('epiphany', 'test', 'asm')
+
+@pytest.mark.parametrize("asm,expected",
+                         [('nop.elf', StateChecker(pc=2)),
+                           pytest.mark.xfail(('add.elf', StateChecker(pc=4)))])
+def test_asm_nop(asm, expected):
+    assert os.getcwd().endswith('pydgin')
     os.putenv('PYTHONPATH', '.')
     child = subprocess.Popen(['python',
                              'epiphany/sim.py',
                               '--debug',
                               'insts,mem,rf,regdump',
-                              asm],
+                              os.path.join(asm_dir, asm)],
                              stdout=subprocess.PIPE)
-    child.communicate()[0]
+    out = child.communicate()[0]
     assert 0 == child.returncode
+    for line in out.split('\n'):
+      if line.startswith("Instructions"):
+        pc = int(line.split()[-1])
+        assert expected.pc == pc
