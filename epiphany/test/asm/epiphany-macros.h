2682c0bf93a008e9fa26a1b693ccc196827a99f4
snim2 <mount.sarah@gmail.com>
2015-07-15 01:36:29 +0100

    ASM files now compiled with gcc. Macros changed to use trap to halt the simulator (TEAR_DOWN) and _main for SET_UP.

--- epiphany/test/asm/epiphany-macros.h
+++ epiphany/test/asm/epiphany-macros.h
@@ -1,8 +1,12 @@
-.macro SET_UP
-.global _start
-_start:
-.endm
+#define TEAR_DOWN trap 0x3
 
-.macro TEAR_DOWN
-    bkpt // Terminate the program by setting a breakpoint.
-.endm
+#define TMP0 r44
+#define TMP1 r45
+#define TMP2 r46
+#define TMP3 r47
+#define TMP4 r48
+#define TMP5 r44
+#define TMP6 r45
+#define TMP7 r46
+#define TMP8 r47
+#define TMP9 r48
--- epiphany/test/test_asm.py
+++ epiphany/test/test_asm.py
@@ -1,4 +1,3 @@
-from epiphany.machine import RESET_ADDR
 from epiphany.sim import Epiphany
 from epiphany.utils import float2bits
 from epiphany.test.machine import StateChecker
@@ -9,36 +8,36 @@ import pytest
 elf_dir = os.path.join('epiphany', 'test', 'asm')
 
 @pytest.mark.parametrize("elf,expected",
-       [('add.elf', StateChecker(pc=(16 + RESET_ADDR), rf1=110, rf2=7, rf3=105)),
-        ('and.elf', StateChecker(pc=(14 + RESET_ADDR), rf0=0, rf1=1, rf2=1, rf3=0, rf4=0, rf5=0)),
-        ('asr.elf', StateChecker(pc=(10 +  + RESET_ADDR), rf0=1, rf1=5, rf2=0, rf3=0)),
-        ('bcond.elf', StateChecker(pc=(12 + RESET_ADDR), rf0=0, rf1=110)),
-        ('bitr.elf', StateChecker(pc=(6 + RESET_ADDR), rf0=170, rf1=85)),
-        ('bl.elf', StateChecker(pc=114, rf0=15, rf1=0, rf2=15)),
+       [('add.elf', StateChecker(rf1=110, rf2=7, rf3=105)),
+        ('and.elf', StateChecker(rf0=0, rf1=1, rf2=1, rf3=0, rf4=0, rf5=0)),
+        ('asr.elf', StateChecker(rf0=1, rf1=5, rf2=0, rf3=0)),
+        ('bcond.elf', StateChecker(rf0=0, rf1=110)),
+        ('bitr.elf', StateChecker(rf0=0x84c2a6e1)),
+        ('bl.elf', StateChecker(rf0=15, rf1=0, rf2=15)),
         pytest.mark.xfail(('dma_transfer.elf', StateChecker())),
-        ('eor.elf', StateChecker(pc=(8 + RESET_ADDR), rf0=5, rf1=7, rf2=2)),
-        ('fix.elf', StateChecker(pc=(8 + RESET_ADDR), rf0=5)),
-        ('gid.elf', StateChecker(pc=(6 + RESET_ADDR), rfSTATUS=0b10)),
-        ('gie.elf', StateChecker(pc=(6 + RESET_ADDR), rfSTATUS=0b00)),
-        ('hardware_loop.elf',
-         StateChecker(pc=(68 + RESET_ADDR), rf0=100, rf1=116, rf2=100, rf3=100,
-                      rf4=100, rf5=100, rf6=100, rf7=100, rf8=100, rfSTATUS=0b00)),
-        ('jalr.elf', StateChecker(pc=(10 + RESET_ADDR), rf3=100, rfLR=0x5e)),
-        ('jr.elf', StateChecker(pc=(14 + RESET_ADDR), rf0=3, rf1=1, rf2=2)),
-        ('low_high.elf', StateChecker(pc=(10 + RESET_ADDR), rf3=0xFFFFFFFF)),
-        ('lsl.elf', StateChecker(pc=(10 + RESET_ADDR), rf0=5, rf1=7, rf2=640, rf3=640)),
-        ('lsr.elf', StateChecker(pc=(10 + RESET_ADDR), rf0=3, rf1=1, rf2=1, rf3=1)),
-        ('mov_cond.elf', StateChecker(pc=(14 + RESET_ADDR), rf0=0, rf1=15, rf2=15, rf3=15)),
-        ('movfs.elf', StateChecker(pc=(10 + RESET_ADDR), rf0=7, rf63=7, rfCONFIG=7)),
-        ('mov_imm.elf', StateChecker(pc=(4 + RESET_ADDR), rf0=25)),
-        ('movt.elf', StateChecker(pc=(6 + RESET_ADDR), rf0=2415919104)),
-        ('movts.elf', StateChecker(pc=(6 + RESET_ADDR), rf0=7, rfCONFIG=7)),
-        ('nop.elf', StateChecker(pc=(4 + RESET_ADDR))),
-        ('orr.elf', StateChecker(pc=(8 + RESET_ADDR), rf0=5, rf1=7, rf2=7)),
-        pytest.mark.xfail(('rti.elf', StateChecker())),
-        ('rts.elf', StateChecker(pc=110, rf1=100, rf2=200, rf3=300, rfLR=90)),
-        ('sub.elf', StateChecker(pc=(12 + RESET_ADDR), rf0=100, rf1=20, rf2=80, rf3=80)),
-        pytest.mark.xfail(('trap.elf', StateChecker(pc=(6 + RESET_ADDR)))),
+        ('eor.elf', StateChecker(rf0=5, rf1=7, rf2=2)),
+        ('fix.elf', StateChecker(rf0=5)),
+        ('gid.elf', StateChecker(rfSTATUS=0b10)),
+        ('gie.elf', StateChecker(rfSTATUS=0b00)),
+        pytest.mark.xfail(('hardware_loop.elf',
+         StateChecker(rf0=100, rf1=116, rf2=100, rf3=100,
+                      rf4=100, rf5=100, rf6=100, rf7=100, rf8=100, rfSTATUS=0b00))),
+        ('jalr.elf', StateChecker(rf3=100, rfLR=0x236)),
+        ('jr.elf', StateChecker(rf0=3, rf1=1, rf2=2)),
+        ('low_high.elf', StateChecker(rf3=0xFFFFFFFF)),
+        ('lsl.elf', StateChecker(rf0=5, rf1=7, rf2=640, rf3=640)),
+        ('lsr.elf', StateChecker(rf0=3, rf1=1, rf2=1, rf3=1)),
+        ('mov_cond.elf', StateChecker(rf0=0, rf1=15, rf2=15, rf3=15)),
+        pytest.mark.xfail(('movfs.elf', StateChecker(rf0=7, rf63=7, rfIRET=7))),
+        ('mov_imm.elf', StateChecker(rf0=25)),
+        ('movt.elf', StateChecker(rf0=2415919104)),
+        ('movts.elf', StateChecker(rf0=7, rfIRET=7)),  # FIXME
+        ('nop.elf', StateChecker(pc=564)),
+        ('orr.elf', StateChecker(rf0=5, rf1=7, rf2=7)),
+        pytest.mark.xfail(('rti.elf', StateChecker())), # FIXME
+        ('rts.elf', StateChecker(rf1=100, rf2=200, rf3=300, rfLR=562)),
+        ('sub.elf', StateChecker(rf0=100, rf1=20, rf2=80, rf3=80)),
+        pytest.mark.xfail(('trap.elf', StateChecker())),
        ])
 def test_elf(elf, expected):
     """Test ELF files that deal in unsigned integers (rather than floats).
@@ -47,80 +46,68 @@ def test_elf(elf, expected):
     epiphany = Epiphany()
     with open(elf_filename, 'rb') as elf:
         epiphany.init_state(elf, elf_filename, '', [], False, is_test=True)
-        epiphany.max_insts = 250
+        epiphany.max_insts = 10000
         epiphany.run()
         expected.check(epiphany.state)
 
 
 @pytest.mark.parametrize("elf,expected",
-                         [('fabs.elf', StateChecker(pc=(24 + RESET_ADDR),
-                                                    rf0=float2bits(5.0),
+                         [('fabs.elf', StateChecker(rf0=float2bits(5.0),
                                                     rf1=float2bits(0.0),
                                                     rf2=float2bits(-5.0),
                                                     rf3=float2bits(5.0),
                                                     rf4=float2bits(0.0),
                                                     rf5=float2bits(5.0),
                                                     )),
-                          ('float.elf', StateChecker(pc=(6 + RESET_ADDR),
-                                                     rf1=float2bits(25.0))),
-                          ('fadd.elf', StateChecker(pc=(18 + RESET_ADDR),
-                                                    rf0=float2bits(15.0),
+                          ('float.elf', StateChecker(rf1=float2bits(25.0))),
+                          ('fadd.elf', StateChecker(rf0=float2bits(15.0),
                                                     rf1=float2bits(5.0),
                                                     rf2=float2bits(5.0),
                                                     rf3=float2bits(10.0),
                                                     rf4=float2bits(0.0))),
-                          ('fsub.elf', StateChecker(pc=(20 + RESET_ADDR),
-                                                    rf0=float2bits(0.0),
+                          ('fsub.elf', StateChecker(rf0=float2bits(0.0),
                                                     rf1=float2bits(2.0),
                                                     rf2=float2bits(5.0),
                                                     rf3=float2bits(5.0),
                                                     rf4=float2bits(3.0),
                                                     rf5=float2bits(-3.0))),
-                          ('fmul.elf', StateChecker(pc=(18 + RESET_ADDR),
-                                                    rf0=float2bits(0.0),
+                          ('fmul.elf', StateChecker(rf0=float2bits(0.0),
                                                     rf1=float2bits(2.0),
                                                     rf2=float2bits(5.0),
                                                     rf3=float2bits(0.0),
                                                     rf4=float2bits(10.0))),
-                          ('fmadd.elf', StateChecker(pc=(26 + RESET_ADDR),
-                                                     rf0=float2bits(0.0),
+                          ('fmadd.elf', StateChecker(rf0=float2bits(0.0),
                                                      rf1=float2bits(2.0),
                                                      rf2=float2bits(5.0),
                                                      rf3=float2bits(17.0),
                                                      rf4=float2bits(7.0))),
-                          ('fmsub.elf', StateChecker(pc=(26 + RESET_ADDR),
-                                                     rf0=float2bits(0.0),
+                          ('fmsub.elf', StateChecker(rf0=float2bits(0.0),
                                                      rf1=float2bits(2.0),
                                                      rf2=float2bits(5.0),
                                                      rf3=float2bits(-3.0),
                                                      rf4=float2bits(7.0))),
-                          ('iadd.elf', StateChecker(pc=(18 + RESET_ADDR),
-                                                    rf0=float2bits(15.0),
+                          ('iadd.elf', StateChecker(rf0=float2bits(15.0),
                                                     rf1=float2bits(5.0),
                                                     rf2=float2bits(5.0),
                                                     rf3=float2bits(10.0),
                                                     rf4=float2bits(0.0))),
-                          ('isub.elf', StateChecker(pc=(20 + RESET_ADDR),
-                                                    rf0=float2bits(0.0),
+                          ('isub.elf', StateChecker(rf0=float2bits(0.0),
                                                     rf1=float2bits(2.0),
                                                     rf2=float2bits(5.0),
                                                     rf3=float2bits(5.0),
                                                     rf4=float2bits(3.0),
                                                     rf5=float2bits(-3.0))),
-                          ('imul.elf', StateChecker(pc=(18 + RESET_ADDR),
-                                                    rf0=float2bits(0.0),
+                          ('imul.elf', StateChecker(rf0=float2bits(0.0),
                                                     rf1=float2bits(2.0),
                                                     rf2=float2bits(5.0),
                                                     rf3=float2bits(0.0),
                                                     rf4=float2bits(10.0))),
-                          ('imadd.elf', StateChecker(pc=(26 + RESET_ADDR),
-                                                     rf0=float2bits(0.0),
+                          ('imadd.elf', StateChecker(rf0=float2bits(0.0),
                                                      rf1=float2bits(2.0),
                                                      rf2=float2bits(5.0),
                                                      rf3=float2bits(17.0),
                                                      rf4=float2bits(7.0))),
-                          ('imsub.elf', StateChecker(pc=(26 + RESET_ADDR),
-                                                     rf0=float2bits(0.0),
+                          ('imsub.elf', StateChecker(rf0=float2bits(0.0),
                                                      rf1=float2bits(2.0),
                                                      rf2=float2bits(5.0),
                                                      rf3=float2bits(-3.0),
@@ -136,20 +123,17 @@ def test_fp_elf(elf, expected):
     epiphany = Epiphany()
     with open(elf_filename, 'rb') as elf:
         epiphany.init_state(elf, elf_filename, '', [], False, is_test=True)
-        epiphany.max_insts = 250
+        epiphany.max_insts = 10000
         epiphany.run()
         expected.fp_check(epiphany.state)
 
 
 @pytest.mark.parametrize("elf,expected",
-       [('ldr_disp.elf',    StateChecker(pc=(12 + RESET_ADDR), rf0=0xFFFFFFFF,
-                                         rf1=0x00100000)),
-        ('ldr_disp_pm.elf', StateChecker(pc=(14 + RESET_ADDR), rf0=0xFFFFFFFF,
-                                         rf1=0x00100004)),
-        ('ldr_index.elf',   StateChecker(pc=(16 + RESET_ADDR), rf0=0xFFFFFFFF,
-                                         rf1=0x00100004, rf2=0)),
-        ('ldr_pm.elf',      StateChecker(pc=(20 + RESET_ADDR), rf0=0xFFFFFFFF,
-                                         rf1=0x00100004, rf2=0x80002)),
+       [('ldr_disp.elf',    StateChecker(rf0=0xFFFFFFFF, rf1=0x00100000)),
+        ('ldr_disp_pm.elf', StateChecker(rf0=0xFFFFFFFF, rf1=0x00100004)),
+        ('ldr_index.elf',   StateChecker(rf0=0xFFFFFFFF, rf1=0x00100004, rf2=0)),
+        ('ldr_pm.elf',      StateChecker(rf0=0xFFFFFFFF, rf1=0x00100004,
+                                         rf2=0x80002)),
        ])
 def test_load(elf, expected):
     """Test ELF files that load values from memory into a register.
@@ -159,16 +143,16 @@ def test_load(elf, expected):
     with open(elf_filename, 'rb') as elf:
         epiphany.init_state(elf, elf_filename, '', [], False, is_test=True)
         epiphany.state.mem.write(0x00100004, 4, 0xFFFFFFFF)
-        epiphany.max_insts = 250
+        epiphany.max_insts = 10000
         epiphany.run()
         expected.check(epiphany.state)
 
 
 @pytest.mark.parametrize("elf,expected",
-       [('str_disp.elf', StateChecker(pc=(20 + RESET_ADDR), rf0=0xFFFFFFFF, rf1=0x00100000)),
-        ('str_disp_pm.elf', StateChecker(pc=(22 + RESET_ADDR), rf0=0xFFFFFFFF, rf1=0x00100004)),
-        ('str_index.elf', StateChecker(pc=(24 + RESET_ADDR), rf0=0xFFFFFFFF, rf1=0x00100004, rf2=0)),
-        ('str_pm.elf', StateChecker(pc=(22 + RESET_ADDR), rf0=0xFFFFFFFF, rf1=0x00100004, rf2=4)),
+       [('str_disp.elf', StateChecker(rf0=0xFFFFFFFF, rf1=0x00100000)),
+        ('str_disp_pm.elf', StateChecker(rf0=0xFFFFFFFF, rf1=0x00100004)),
+        ('str_index.elf', StateChecker(rf0=0xFFFFFFFF, rf1=0x00100004, rf2=0)),
+        ('str_pm.elf', StateChecker(rf0=0xFFFFFFFF, rf1=0x00100004, rf2=4)),
        ])
 def test_store(elf, expected):
     """Test ELF files that transfer data from registers to memory.
@@ -177,8 +161,7 @@ def test_store(elf, expected):
     epiphany = Epiphany()
     with open(elf_filename, 'rb') as elf:
         epiphany.init_state(elf, elf_filename, '', [], False, is_test=True)
-        epiphany.state.mem.write(0x00100004, 4, 0xFFFFFFFF)
-        epiphany.max_insts = 250
+        epiphany.max_insts = 10000
         epiphany.run()
         expected.check(epiphany.state, memory=[(0x00100004, 4, 0xFFFFFFFF)])
 
@@ -190,9 +173,9 @@ def test_testset32():
         if elf == 'movts.elf': print 'MOVTS'
         epiphany.init_state(elf, elf_filename, '', [], False, is_test=True)
         epiphany.state.mem.write(0x100004, 4, 0x0)
-        epiphany.max_insts = 10
+        epiphany.max_insts = 100000
         epiphany.run()
-        expected = StateChecker(pc=(22 + RESET_ADDR), AZ=1, rf0=0, rf1=0x100000, rf2=0x4)
+        expected = StateChecker(AZ=1, rf0=0, rf1=0x100000, rf2=0x4)
         expected.check(epiphany.state, memory=[(0x100004, 4, 0xFFFF)])
 
 
@@ -209,7 +192,7 @@ within the on-chip local memory and must be greater than 0x00100000 (2^20).
         epiphany = Epiphany()
         with open(elf_filename, 'rb') as elf:
             epiphany.init_state(elf, elf_filename, '', [], False, is_test=True)
-            epiphany.max_insts = 10
+            epiphany.max_insts = 100000
             epiphany.run()
     assert expected_text == expected_exn.value.message
 
@@ -222,10 +205,10 @@ def test_execute_idle16(capsys):
     with open(elf_filename, 'rb') as elf:
         epiphany.init_state(elf, elf_filename, '', [], False, is_test=True)
         epiphany.state.rfSTATUS = 1
-        epiphany.max_insts = 250
+        epiphany.max_insts = 10000
         epiphany.run()
         out, err = capsys.readouterr()
-        expected_state = StateChecker(pc=(4 + RESET_ADDR), rfSTATUS=0)
+        expected_state = StateChecker(rfSTATUS=0)
         expected_text = ('IDLE16 does not wait in this simulator. ' +
                          'Moving to next instruction.')
         expected_state.check(epiphany.state)
