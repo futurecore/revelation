6534c7bb1638613db22740c427a9cee89df85405
Sarah Mount <s.mount@wlv.ac.uk>
2015-07-26 02:01:03 +0100

    Use correct operand to calculate overflow

--- epiphany/execute_iarith.py
+++ epiphany/execute_iarith.py
@@ -24,12 +24,13 @@ def make_add_executor(is16bit):
         """
         if is16bit:
             inst.bits &= 0xffff
-        result = signed(s.rf[inst.rn]) + reg_or_imm(s, inst, is16bit)
+        op2 = reg_or_imm(s, inst, is16bit)
+        result = signed(s.rf[inst.rn]) + op2
         s.rf[inst.rd] = trim_32(result)
         s.AN = bool((result >> 31) & 1)
         s.AZ = True if trim_32(result) == 0 else False
         s.AC = carry_from(result)
-        s.AV = bool(overflow_from_add(s.rf[inst.rn], s.rf[inst.rm], result))
+        s.AV = bool(overflow_from_add(s.rf[inst.rn], op2, result))
         s.AVS = s.AVS or s.AV
         s.debug_flags()
         s.pc += 2 if is16bit else 4
@@ -44,7 +45,7 @@ def make_sub_executor(is16bit):
         """
         RD = RN - <OP2>
         AN = RD[31]
-        AC = BORROW
+        AC = ~BORROW
         if (RD[31:0]==0) { AZ=1 } else { AZ=0}
         if ((RD[31] & ~RM[31] & RN[31]) | (RD[31] & ~RM[31] & RN[31]) )
         { AV=1 }
@@ -53,12 +54,13 @@ def make_sub_executor(is16bit):
         """
         if is16bit:
             inst.bits &= 0xffff
-        result = signed(s.rf[inst.rn]) - reg_or_imm(s, inst, is16bit)
+        op2 = reg_or_imm(s, inst, is16bit)
+        result = signed(s.rf[inst.rn]) - op2
         s.rf[inst.rd] = trim_32(result)
         s.AN = bool((result >> 31) & 1)
         s.AC = not borrow_from(result)
         s.AZ = True if trim_32(result) == 0 else False
-        s.AV = bool(overflow_from_sub(s.rf[inst.rn], s.rf[inst.rm], result))
+        s.AV = bool(overflow_from_sub(s.rf[inst.rn], op2, result))
         s.AVS = s.AVS or s.AV
         s.debug_flags()
         s.pc += 2 if is16bit else 4
