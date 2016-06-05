1a142ba9535a6c0f4d43074a4c11e0486e25b499
Sarah Mount <sarah.mount@kcl.ac.uk>
2016-06-05 19:14:31 +0100

    Automatically deploy Revelation binaries to GitHub. Remove Slack notification. Fixes #20.

---
+++ scripts/deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -ev
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ] && [ `ls ${TRAVIS_BUILD_DIR}/bin/* 2>/dev/null | wc -l` = "2" ]; then
+    echo -e "Deploying to revelation-bins.\n"
+
+    # Configure git.
+    git config --global user.email "travis-ci@travis-ci.org"
+    git config --global user.name "Travis CI"
+
+    # Clone revelation binaries repository.
+    mkdir ${HOME}/revelation-bins
+    cd ${HOME}/revelation-bins
+    git init
+    git pull https://$GH_TOKEN@github.com/futurecore/revelation-bins.git
+
+    # Copy binary simulators into new repository.
+    cp ${TRAVIS_BUILD_DIR}/bin/pydgin-revelation* .
+
+    # Add, commit and push binary files.
+    git add pydgin-revelation*
+    git commit -m "Travis build ${TRAVIS_BUILD_NUMBER} pushed to master"
+    git push --force --quiet https://$GH_TOKEN@github.com/futurecore/revelation-bins master
+
+    echo -e "Deployed to revelation-bins.\n"
+fi
