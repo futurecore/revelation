98556f67763207e4c213dff2b36b1b3d18e0d23b
Sarah Mount <sarah.mount@kcl.ac.uk>
2016-06-05 15:29:27 +0100

    Simpify condition codes.

--- epiphany/condition_codes.py
+++ epiphany/condition_codes.py
@@ -5,38 +5,24 @@ except ImportError:
 
 
 def condition_passed(s, cond):
-    if cond == 0b0000:    # BEQ
-        return s.AZ
-    elif cond == 0b0001:  # BNE
-        return not s.AZ
-    elif cond == 0b0010:  # BGTU
-        return (not s.AZ) and s.AC
-    elif cond == 0b0011:  # BGTEU
-        return s.AC
-    elif cond == 0b0100:  # BLTEU
-        return s.AZ or (not s.AC)
-    elif cond == 0b0101:  # BLTU
-        return not s.AC
-    elif cond == 0b0110:  # BGT
-        return (not s.AZ) and (s.AV == s.AN)
-    elif cond == 0b0111:  # BGTE
-        return s.AV == s.AN
-    elif cond == 0b1000:  # BLT
-        return s.AV != s.AN
-    elif cond == 0b1001:  # BLTE
-        return s.AZ or (s.AV != s.AN)
-    elif cond == 0b1010:  # BBEQ
-        return s.BZ
-    elif cond == 0b1011:  # BBNE
-        return not s.BZ
-    elif cond == 0b1100:  # BBLT
-        return s.BN and (not s.BZ)
-    elif cond == 0b1101:  # BBLTE
-        return s.BN or s.BZ
-    elif cond == 0b1110:  # B (unconditional branch)
-        return True
-    elif cond == 0b1111:  # BL (branch and link)
-        return True
+    condtions = { 0b0000: s.AZ,                          # BEQ
+                  0b0001: not s.AZ,                      # BNE
+                  0b0010: (not s.AZ) and s.AC,           # BGTU
+                  0b0011: s.AC,                          # BGTEU
+                  0b0100: s.AZ or (not s.AC),            # BLTEU
+                  0b0101: not s.AC,                      # BLTU
+                  0b0110: (not s.AZ) and (s.AV == s.AN), # BGT
+                  0b0111: s.AV == s.AN,                  # BGTE
+                  0b1000: s.AV != s.AN,                  # BLT
+                  0b1001: s.AZ or (s.AV != s.AN),        # BLTE
+                  0b1010: s.BZ,                          # BBEQ
+                  0b1011: not s.BZ,                      # BBNE
+                  0b1100: s.BN and (not s.BZ),           # BBLT
+                  0b1101: s.BN or s.BZ,                  # BBLTE
+                  0b1110: True,  # B (unconditional branch)
+                  0b1111: True,  # BL (branch and link)
+    }
+        return condtions[cond]
     else:
         if we_are_translated():
             raise ValueError
