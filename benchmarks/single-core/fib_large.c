5e7b58d858b84d39f9fe9ff28569daccfb6e8c27
Sarah Mount <sarah.mount@kcl.ac.uk>
2016-08-07 21:18:28 +0100

    Bump number of iterations in benchmarks.

--- benchmarks/single-core/fib_large.c
+++ benchmarks/single-core/fib_large.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 int main() {
     unsigned long a = 1, b = 1, i = 0, temp = 0;
-    for(i = 0; i < 10000; i++) {
+    for(i = 0; i < 10000000; i++) {
         temp = a;
         a = b;
         b += temp;
